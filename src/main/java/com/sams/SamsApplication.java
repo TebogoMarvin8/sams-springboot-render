153f666b93be3c14adf62ff5764c520989827a4b
Tebogo Marvin Malogadothlare <tebogomarvin8@gmail.com>
2024-10-04 15:46:34 +0200

    added extension on main

--- src/main/java/com/sams/SamsApplication.java
+++ src/main/java/com/sams/SamsApplication.java
@@ -2,9 +2,10 @@ package com.sams;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SamsApplication {
+public class SamsApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(SamsApplication.class, args);
     }
